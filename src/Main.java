06925cca2b2f73dfd120fa46c6156071bf81ee7c
Kibriar Hossain <shantohossain.p@gmail.com>
2022-10-15 02:31:45 +0600

    initial version

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,4 @@
-import java.util.Arrays;
+
 
 public class Main {
     public static void main(String[] args) {
@@ -16,14 +16,6 @@ public class Main {
 
         System.out.println("Processor Cache = "+processor.getCache());
         System.out.println("Ram clock speed = "+ram.getClockSpeed());
-
-
-        String[] names = new String[5];
-        System.out.println(Arrays.toString(names));
-
-        names[0] = "Shanto";
-        System.out.println(Arrays.toString(names));
-
 
     }
 }
